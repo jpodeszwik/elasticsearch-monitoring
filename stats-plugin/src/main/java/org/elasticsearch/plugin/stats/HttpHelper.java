bad9470e45b29a3a29e34806dd38e71fd7ec99c7
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2014-08-26 22:37:27 +0200

    Added more loggers

--- stats-plugin/src/main/java/org/elasticsearch/plugin/stats/HttpHelper.java
+++ stats-plugin/src/main/java/org/elasticsearch/plugin/stats/HttpHelper.java
@@ -6,6 +6,8 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.ESLoggerFactory;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -20,6 +22,8 @@ import java.nio.charset.Charset;
  * Date: 09/08/14
  */
 public class HttpHelper {
+    private ESLogger logger = ESLoggerFactory.getLogger("plugin.stats.HttpHelper");
+
     public void postString(String url, String data) throws IOException {
         HttpClient httpClient = HttpClientBuilder.create().build();
 
@@ -27,6 +31,10 @@ public class HttpHelper {
             HttpPost request = new HttpPost(url);
             request.setEntity(new StringEntity(data));
             HttpResponse response = httpClient.execute(request);
+            logger.debug("postString status code: " + response.getStatusLine().getStatusCode());
+        } catch (IOException e) {
+            logger.error("postString problem with sending http request");
+            throw e;
         } finally {
             httpClient.getConnectionManager().shutdown();
         }
@@ -56,10 +64,12 @@ public class HttpHelper {
             HttpGet request = new HttpGet(url);
             HttpResponse response = httpClient.execute(request);
 
-            InputStream inputStream = response.getEntity().getContent();
+            logger.debug("performHttpRequest status code: " + response.getStatusLine().getStatusCode());
 
+            InputStream inputStream = response.getEntity().getContent();
             return readContentFromInputStream(inputStream);
         } catch (IOException e) {
+            logger.error("performHttpRequest Problem with sending http request", e);
             throw e;
         } finally {
             httpClient.getConnectionManager().shutdown();
